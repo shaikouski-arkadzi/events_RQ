feb3cce6e9bc25d1ad36d2e18d731c4ed7913d5a
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2024-03-06 09:53:43 +0300

    get events using tanstack query

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Events from './components/Events/Events.jsx';
 import EventDetails from './components/Events/EventDetails.jsx';
 import NewEvent from './components/Events/NewEvent.jsx';
 import EditEvent from './components/Events/EditEvent.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const router = createBrowserRouter([
   {
@@ -37,8 +38,14 @@ const router = createBrowserRouter([
   },
 ]);
 
-function App() {
-  return <RouterProvider router={router} />;
+const queryClient = new QueryClient();
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
---
+++ src/util/http.js
@@ -0,0 +1,18 @@
+import { getDocs, collection } from 'firebase/firestore';
+import { getDownloadURL, ref } from 'firebase/storage';
+import { db, storage } from '../firebase';
+
+export const getEvents = async () => {
+  try {
+    const collectionRef = collection(db, 'events');
+    const response = await getDocs(collectionRef);
+    const filteredData = await Promise.all(response.docs.map(async doc => ({
+      ...doc.data(),
+      image: await getDownloadURL(ref(storage, doc.data().image)),
+      id: doc.id
+    })));
+    return filteredData;
+  } catch (err) {
+    throw JSON.parse(JSON.stringify(err));
+  }
+}
